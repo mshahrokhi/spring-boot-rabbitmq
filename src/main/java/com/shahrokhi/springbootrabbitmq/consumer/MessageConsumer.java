63504416ee17247b17a47c5a2292a92defd717ed
mohammad shahrokhi <mohammad.shahrokhi72@gmail.com>
2023-11-14 18:42:23 +0330

    Rename consumer class

--- src/main/java/com/shahrokhi/springbootrabbitmq/consumer/MessageConsumer.java
+++ src/main/java/com/shahrokhi/springbootrabbitmq/consumer/MessageConsumer.java
@@ -7,9 +7,9 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SendSmsConsumer {
+public class MessageConsumer {
 
-    private final Logger logger = LogManager.getLogger(SendSmsConsumer.class);
+    private final Logger logger = LogManager.getLogger(MessageConsumer.class);
 
     @RabbitListener(queues = "${rabbitmq.queue.name.send-sms}")
     public void sendSmsConsumer(MessageRequest request) {
