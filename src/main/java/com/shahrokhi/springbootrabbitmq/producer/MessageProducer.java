eeca5a33df0522ca19375d3f14653648e466e070
mohammad shahrokhi <mohammad.shahrokhi72@gmail.com>
2023-11-14 12:32:45 +0330

    Add interface for MessageProducer class

--- src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
+++ src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
@@ -1,41 +1,8 @@
 package com.shahrokhi.springbootrabbitmq.producer;
 
 import com.shahrokhi.springbootrabbitmq.model.MessageRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
-public class MessageProducer {
-
-    private final Logger logger = LogManager.getLogger(MessageProducer.class);
-
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
-
-    @Value("${rabbitmq.queue.routing-key.send-sms}")
-    private String sendSmsRoutingKey;
-
-    @Value("${rabbitmq.queue.routing-key.send-email}")
-    private String sendEmailRoutingKey;
-
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public MessageProducer(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
-    public void sendSms(MessageRequest request) {
-        logger.info(String.format("Producer SMS sent -> %s", request));
-        rabbitTemplate.convertAndSend(exchangeName, sendSmsRoutingKey, request);
-    }
-
-    public void sendEmail(MessageRequest request) {
-        logger.info(String.format("Producer email sent -> %s", request));
-        rabbitTemplate.convertAndSend(exchangeName, sendEmailRoutingKey, request);
-    }
+public interface MessageProducer {
+    public void sendSms(MessageRequest request);
+    public void sendEmail(MessageRequest request);
 }
---
+++ src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducerImp.java
@@ -0,0 +1,41 @@
+package com.shahrokhi.springbootrabbitmq.producer;
+
+import com.shahrokhi.springbootrabbitmq.model.MessageRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageProducerImp implements MessageProducer {
+
+    private final Logger logger = LogManager.getLogger(MessageProducerImp.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.queue.routing-key.send-sms}")
+    private String sendSmsRoutingKey;
+
+    @Value("${rabbitmq.queue.routing-key.send-email}")
+    private String sendEmailRoutingKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public MessageProducerImp(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendSms(MessageRequest request) {
+        logger.info(String.format("Producer SMS sent -> %s", request));
+        rabbitTemplate.convertAndSend(exchangeName, sendSmsRoutingKey, request);
+    }
+
+    public void sendEmail(MessageRequest request) {
+        logger.info(String.format("Producer email sent -> %s", request));
+        rabbitTemplate.convertAndSend(exchangeName, sendEmailRoutingKey, request);
+    }
+}
