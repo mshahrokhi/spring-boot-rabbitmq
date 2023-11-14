e66a56beda9f6d7da00cf7d638c46ebc7e75533c
mohammad shahrokhi <mohammad.shahrokhi72@gmail.com>
2023-11-14 12:23:29 +0330

    Change the MessageProducerClass to Component

--- src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
+++ src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class MessageProducer {
 
     private final Logger logger = LogManager.getLogger(MessageProducer.class);
