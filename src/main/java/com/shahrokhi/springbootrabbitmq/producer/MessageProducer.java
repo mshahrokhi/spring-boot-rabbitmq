5be76363e152d79ab08135d8bc46758c869bcd9e
mohammad shahrokhi <mohammad.shahrokhi72@gmail.com>
2023-11-14 12:40:08 +0330

    Add interface for MessageProducer class

--- src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
+++ src/main/java/com/shahrokhi/springbootrabbitmq/producer/MessageProducer.java
@@ -3,6 +3,6 @@ package com.shahrokhi.springbootrabbitmq.producer;
 import com.shahrokhi.springbootrabbitmq.model.MessageRequest;
 
 public interface MessageProducer {
-    public void sendSms(MessageRequest request);
-    public void sendEmail(MessageRequest request);
+    void sendSms(MessageRequest request);
+    void sendEmail(MessageRequest request);
 }
